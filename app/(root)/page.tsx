e375165ac8a23cac684691d714af3d2953395326
M-Abdullah-Nazeer <nazeermuhammadabdullah@gmail.com>
2025-02-06 16:10:13 -0600

    working on sample product

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = () => {
-  return (<></>);
+  console.log(sampleData);
+
+  return (<>
+
+    <ProductList data={sampleData.products} title="Newest Arrival" limit={4} />
+
+  </>
+
+  );
 }
 
 export default HomePage;
