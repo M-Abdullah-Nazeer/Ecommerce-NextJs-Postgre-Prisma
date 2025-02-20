a054fb2cc0461fccdf6dbc398ddb4ba2dd1587a9
M-Abdullah-Nazeer <nazeermuhammadabdullah@gmail.com>
2025-02-20 15:37:47 -0600

    working on signup form

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,66 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+
+
+export const metadata: Metadata = {
+    title: 'Sign Up',
+};
+
+
+
+const SignUpPage = async (props: {
+    searchParams: Promise<{
+        callbackUrl: string
+    }>
+}) => {
+
+    const session = await auth();
+
+    const { callbackUrl } = await props.searchParams;
+
+    if (session) {
+        return redirect(callbackUrl || '/')
+    }
+
+    return (
+        <div className="w-full max-w-md mx-auto">
+
+            <Card>
+                <CardHeader className="space-y-4">
+
+                    <Link className="flex-center" href='/'>
+                        <Image
+                            src='/images/logo.svg'
+                            alt={`${APP_NAME} logo`}
+                            width={100}
+                            height={100}
+                            priority={true}
+                        />
+
+                    </Link>
+                    <CardTitle className="text-center">
+                        Sign Up
+                    </CardTitle>
+
+                    <CardDescription className="text-center">
+                        Enter you details below to create account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                    {/* FORM COMPONENT IS HERE */}
+
+                </CardContent>
+            </Card>
+
+        </div>
+    );
+};
+
+
+export default SignUpPage;
